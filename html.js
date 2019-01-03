a18e97238a0bbf38e60130ad328fac148a98f659
GreenEyes <flddnick@gmail.com>
2019-01-03 10:21:03 -0600

    updated layout

---
+++ html.js
@@ -0,0 +1,5 @@
+// for friend finder
+
+// create an interface that allows user to fill out questions
+
+// store user data on DB and allow for data match to be established to find similar alike users
